b6f6dc43ab21449a86c9019e091fd410c9a42305
jonlee012 <jonlee86.jl@gmail.com>
2022-06-08 12:02:01 -0500

    albums

--- albums_migration.sql
+++ albums_migration.sql
@@ -3,13 +3,13 @@ USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
 CREATE TABLE IF NOT EXISTS albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist_name VARCHAR(50) DEFAULT 'NONE',
+    artist VARCHAR(50) DEFAULT 'NONE',
     record_name  VARCHAR(100) NOT NULL,
     release_date INT UNSIGNED,
-    sales_mil FLOAT,
+    sales FLOAT,
     genre TEXT NOT NULL,
     PRIMARY KEY (id),
-    UNIQUE (artist_name, record_name)
+    UNIQUE (artist, record_name)
 );
 -- DESCRIBE albums;
 --
