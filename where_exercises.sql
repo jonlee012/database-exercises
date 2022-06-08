ed0d6a9fa254854ccea080e75653528805f5868e
jonlee012 <jonlee86.jl@gmail.com>
2022-06-08 12:29:39 -0500

    employees

--- where_exercises.sql
+++ where_exercises.sql
@@ -14,6 +14,15 @@ WHERE last_name LIKE '%q%';
 
 SELECT *
 FROM employees
+WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya';
+
+SELECT *
+FROM employees
+WHERE (first_name  = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya')
+AND gender = 'M';
+
+SELECT *
+FROM employees
 WHERE last_name LIKE '%E'
    OR last_name LIKE 'E%';
 
