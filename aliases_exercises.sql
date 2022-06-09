6b9042a762dfb1718cd10870764d5610ba1f5159
jonlee012 <jonlee86.jl@gmail.com>
2022-06-09 14:30:49 -0500

    employees

--- aliases_exercises.sql
+++ aliases_exercises.sql
@@ -4,3 +4,8 @@ SELECT CONCAT(emp_no, ' - ', last_name, ', ', first_name)
 AS full_name, birth_date AS DOB
 FROM employees
 LIMIT 10;
+
+DESCRIBE employees;
+
+# ALTER TABLE employees
+#     ADD UNIQUE (birth_date);
