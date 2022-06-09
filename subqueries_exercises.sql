f13cdd5662d47c26a45893ebc2ab78b60bd7a43e
jonlee012 <jonlee86.jl@gmail.com>
2022-06-09 14:31:00 -0500

    employees

---
+++ subqueries_exercises.sql
@@ -0,0 +1,49 @@
+USE employees;
+
+SHOW TABLES;
+
+SELECT first_name, hire_date
+FROM employees
+WHERE emp_no IN (
+    SELECT emp_no
+    FROM dept_emp
+    WHERE hire_date = '1990-10-22'
+);
+
+SELECT first_name, last_name
+FROM employees
+WHERE hire_date IN (
+    SELECT hire_date
+    FROM employees
+    WHERE emp_no = '101010'
+);
+
+SELECT * FROM titles
+WHERE emp_no IN (
+    select emp_no from employees where first_name = 'Aamod'
+);
+
+SELECT first_name, last_name
+FROM employees
+WHERE emp_no IN(
+    SELECT emp_no
+    FROM dept_manager
+    WHERE gender = 'f' AND to_date > now()
+);
+
+SELECT first_name, last_name
+FROM employees
+WHERE emp_no IN(
+    SELECT emp_no
+    FROM salaries
+    WHERE salary > 155555
+);
+
+SELECT first_name, last_name
+FROM employees
+WHERE emp_no IN(
+    SELECT emp_no
+    FROM salaries
+    WHERE salary < (SELECT MAX(SALARY) FROM employees)
+    );
+
